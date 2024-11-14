15b46f263e5e272bca0361b359a8c1d674dd6ee3
STORMTR00P3R <noorblum16@gmail.com>
2024-11-13 17:30:10 -0800

    refactor

--- app.js
+++ app.js
@@ -1,70 +1,37 @@
-const balance = document.getElementById("balance");
-const input = document.getElementById("input");
+const balanceLabel = document.getElementById("balance");
+const amountInput = document.getElementById("amount");
+const amountType = document.getElementById("type");
 const addBtn = document.getElementById("add-btn");
 const expensesList = document.getElementById("expenses-list");
-const dd = document.getElementById("type");
-const states = document.getElementById("states");
-
-console.log(dd.value)
 
 class Account {
-    
     constructor() {
         this.balance = 2500.00.toFixed(2);
-        balance.textContent = this.balance;
+        balanceLabel.textContent = this.balance;
     }
-
-    addExpense() {
-        let loss = parseFloat(input.value);
-        this.balance -= loss;
-        console.log(this.balance)
-
-        let expenseItem = document.createElement("li");
-        expenseItem.style.color = "red";
-        expenseItem.textContent = "-$" + loss.toFixed(2);
-        expensesList.appendChild(expenseItem);
-
-        balance.textContent = this.balance.toFixed(2);
-        input.value = "";
-    }
-
-    addIncome() {
-        let gain = parseFloat(input.value);
-        this.balance += gain;
-        console.log(this.balance)
-
-        let incomeItem = document.createElement("li");
-        incomeItem.style.color = "green";
-        incomeItem.textContent = "+$" + gain.toFixed(2);
-        expensesList.appendChild(incomeItem);
-
-        balance.textContent = this.balance.toFixed(2);
-        input.value = "";
-    }
-
-    setBalance(newBalance) {
-        this.balance = newBalance.toFixed(2);
-        balance.textContent = this.balance;
-    }
+		
+		updateBalance() {
+			let amount = parseFloat(amountInput.value);
+			let type = amountType.value;
+			let item = document.createElement("li");
+			let prefix = "";
+			
+			if (type == "loss") {
+				this.balance -= amount;
+				item.style.color = "red";
+				prefix = "-$";
+			} else {
+				this.balance += amount;
+				item.style.color = "green";
+				prefix = "+$";
+			}
+			
+			item.textContent = prefix + amount.toFixed(2);
+			expensesList.appendChild(item);
+			balanceLabel.textContent = this.balance.toFixed(2);
+			amountInput.value = "";
+		}
 }
 
 const account = new Account();
-
-states.addEventListener('change', function() {
-    // const californiaIncome = statesIncome.find(state => state.state === "California");
-    let activeState = statesIncome.find(stateObject => stateObject.state === states.value);
-    account.setBalance(activeState.avg);
-})
-
-addBtn.addEventListener('click', function() {
-    if(input.value != "") {
-        console.log("nothing")
-        if(dd.value == "spent") {
-            account.addExpense();
-        } else {
-            account.addIncome();
-        }
-    } 
-    console.log("woo");
-    console.log(dd.value);
-});
+addBtn.addEventListener("click", account.updateBalance.bind(account));
