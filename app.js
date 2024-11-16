c984d067f48d912145cab74518b4e8b49f02bf3a
STORMTR00P3R <noorblum16@gmail.com>
2024-11-15 18:29:36 -0800

    push

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const amountInput = document.getElementById("amount");
 const amountType = document.getElementById("type");
 const addBtn = document.getElementById("add-btn");
 const expensesList = document.getElementById("expenses-list");
+const states = document.getElementById("states");
+
+generateDrop();
 
 class Account {
     constructor() {
@@ -31,7 +34,29 @@ class Account {
 			balanceLabel.textContent = this.balance.toFixed(2);
 			amountInput.value = "";
 		}
+
+        locationChange(newbalance) {
+            this.balance = newbalance.toFixed(2);
+            balanceLabel.textContent = this.balance;
+        }
 }
 
 const account = new Account();
 addBtn.addEventListener("click", account.updateBalance.bind(account));
+states.addEventListener("change", function() {
+    console.log(states.value);
+    // state1 is not the state in states.js its the object
+    let foundState = statesIncome.find(function(state1) {
+        return states.value == state1.state
+    })
+    account.locationChange(foundState.avg);
+});
+
+function generateDrop() {
+    for(let i = 0; i < statesIncome.length; i++) {
+        let opt = document.createElement("option");
+        opt.value = statesIncome[i].state;
+        opt.textContent = statesIncome[i].state;
+        states.appendChild(opt);
+    }
+}
